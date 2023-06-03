6b118eb5607a1ae720f9ed0d934d28702f000674
Justin <jgoodhcg@gmail.com>
2023-06-02 20:07:02 -0400

    Movable bunny

--- src/my_second_game/core.cljs
+++ src/my_second_game/core.cljs
@@ -2,6 +2,25 @@
 
 (defn ^:export main []
   (let [app (js/PIXI.Application. #js {:backgroundColor 0x1099bb})
-        basic-text (js/PIXI.Text. "Hello Pixi!")]
-    (.addChild (.-stage app) basic-text)
-    (.append (.-body js/document) (.-view app))))
+        texture (js/PIXI.Texture.from "https://pixijs.io/examples/examples/assets/bunny.png")
+        bunny (js/PIXI.Sprite. texture)
+        speed 5] ;; Set the speed of character's movement
+
+    (set! (.-x bunny) 0)
+    (set! (.-y bunny) 0)
+
+    ;; Adding the sprite to the stage
+    (.addChild (.-stage app) bunny)
+
+    ;; Adding the application view to the document body
+    (.append (.-body js/document) (.-view app))
+
+    ;; Adding keyboard event listeners
+    (js/window.addEventListener "keydown" (fn [event]
+                                              (let [code (.-code event)]
+                                                (condp = code
+                                                  "ArrowUp"    (set! (.-y bunny) (- (.-y bunny) speed))
+                                                  "ArrowDown"  (set! (.-y bunny) (+ (.-y bunny) speed))
+                                                  "ArrowLeft"  (set! (.-x bunny) (- (.-x bunny) speed))
+                                                  "ArrowRight" (set! (.-x bunny) (+ (.-x bunny) speed))))))
+))
