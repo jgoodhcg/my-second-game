a5b0cc5bef94834850da48ae383b252972981881
Justin <jgoodhcg@gmail.com>
2023-06-02 21:35:20 -0400

    Basic game loop

--- src/my_second_game/core.cljs
+++ src/my_second_game/core.cljs
@@ -1,13 +1,41 @@
 (ns my-second-game.core)
 
+(def speed 5)
+(def state (atom {:bunny {:x 0 :y 0 :vx 0 :vy 0}}))
+
+(defn update-state []
+  (swap! state update :bunny (fn [bunny]
+                               (let [{:keys [x y vx vy]} bunny
+                                     new-x (+ x vx)
+                                     new-y (+ y vy)]
+                                 (assoc bunny :x new-x :y new-y :vx 0 :vy 0)))))
+
+(defn render [app bunny]
+  (let [{:keys [x y]} (:bunny @state)]
+    (set! (.-x bunny) x)
+    (set! (.-y bunny) y)))
+
+(defn game-loop [app bunny]
+  (fn []
+    (update-state)
+    (render app bunny)
+    (js/requestAnimationFrame (game-loop app bunny))))
+
+(defn handle-keydown [event]
+  (let [code (.-code event)]
+    (swap! state update :bunny
+           (fn [bunny]
+             (condp = code
+               "ArrowUp"    (assoc bunny :vy (- speed))
+               "ArrowDown"  (assoc bunny :vy speed)
+               "ArrowLeft"  (assoc bunny :vx (- speed))
+               "ArrowRight" (assoc bunny :vx speed)
+               bunny)))))
+
 (defn ^:export main []
   (let [app (js/PIXI.Application. #js {:backgroundColor 0x1099bb})
         texture (js/PIXI.Texture.from "https://pixijs.io/examples/examples/assets/bunny.png")
-        bunny (js/PIXI.Sprite. texture)
-        speed 5] ;; Set the speed of character's movement
-
-    (set! (.-x bunny) 0)
-    (set! (.-y bunny) 0)
+        bunny (js/PIXI.Sprite. texture)]
 
     ;; Adding the sprite to the stage
     (.addChild (.-stage app) bunny)
@@ -16,11 +44,7 @@
     (.append (.-body js/document) (.-view app))
 
     ;; Adding keyboard event listeners
-    (js/window.addEventListener "keydown" (fn [event]
-                                              (let [code (.-code event)]
-                                                (condp = code
-                                                  "ArrowUp"    (set! (.-y bunny) (- (.-y bunny) speed))
-                                                  "ArrowDown"  (set! (.-y bunny) (+ (.-y bunny) speed))
-                                                  "ArrowLeft"  (set! (.-x bunny) (- (.-x bunny) speed))
-                                                  "ArrowRight" (set! (.-x bunny) (+ (.-x bunny) speed))))))
-))
+    (js/window.addEventListener "keydown" handle-keydown)
+
+    ;; Start game loop
+    ((game-loop app bunny))))
